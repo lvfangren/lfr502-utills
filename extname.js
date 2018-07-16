c3f98ce91de1889bc4c5c846d86ce3f9da78913a
lvfangren <m18188267414@163.com>
2018-07-16 21:54:19 +0800

    2018/7/16

---
+++ extname.js
@@ -0,0 +1,12 @@
+// 获取文件扩展名
+// 参数：字符串（文件名）
+// 返回：字符串（文件扩展名）
+// 注：对隐藏文件名无用
+
+const extname=(filename)=>{
+  let tmp=filename.split('.');
+  let tmplen=tmp.length;
+  if(tmplen===1||filename.indexOf('.')===0)
+    return '';
+  return tmp[tmplen-1]?('.'+tmp[tmplen-1]):'';
+}
---
+++ toCamelCaseVar.js
@@ -0,0 +1,12 @@
+// 变量的下划线转驼峰命名法
+// 参数：字符串
+// 函数返回值：字符串
+const toCamelCaseVar=(str)=>{
+  str.replace(/_+[a-zA-Z]/,(m,i)=>{
+    if (i) {
+      return m.match(/[a-zA-Z]/)[0].toUpperCase();
+    }else{
+      return m;
+    }
+  })
+}
