8d0ecb52c94b1a9b305b5ac136f024185581f7c8
lvfangren <m18188267414@163.com>
2018-10-26 16:58:55 +0800

    add format 18/10/26

---
+++ format.js
@@ -0,0 +1,22 @@
+// 参考 Dean Edwards 写的 base2 这个库中的 format 方法
+// 功能类似bash脚本中引用（$0,$1，$2...）命令行带的参数
+//
+
+// 注：箭头函数不支持arguments（Firefox好像支持）
+// const format=()=>{
+//   console.log(arguments);
+// }
+
+function format(string) {
+	var args = arguments;
+  // console.log(string,args)
+  // 不能用面向字面量的方式写正则了。
+  var pattern = new RegExp("%([1-" + arguments.length + "])", "g");
+  return String(string).replace(pattern, function(match, index) {
+		return args[index];
+	});
+};
+
+
+
+console.log(format("And the %1 want to know whose %2 you %4", "papers", "shirt", "wear"));
--- tmp.js
+++ tmp.js
@@ -1,12 +1,120 @@
-const highlight = (literals, ...values) => {
-  let result='';
-  console.log(values[0]);
-  for (var i = 0; i < values.length; i++) {
-    result+=literals[i]+`<span class="red">${values[i]}</span>`
+// test1
+// new Promise(function(resolve,reject){
+//   if (true) {
+//     resolve()
+//   }else {
+//     reject()
+//   }
+// }).then(function(){console.log('success');},function(){console.log('faild');}).then(console.log(123)).catch()
+//
 
-  }
-  return result+=literals[i];
-  // return tmp1.replace(/\${+\w+}/g,'<span class="col"></span>');
-}
-// console.log(isSameSet('Hello, ${yourName}. I am ${myName}.');
-console.log(highlight('Hello, ${yourName}. I am ${myName}.',[{a:'s'},{b:21}]));
+
+// test2
+// new Promise(resolve=>{
+//   setTimeout(()=>{
+//     resolve('i am 1');
+//   },2000)
+// }).then(value=>{
+//   console.log(value+'-------test1--------');
+//   return new Promise(resolve=>{
+//     resolve('i am 2');
+//   },2000)
+// }).then((value)=>{
+//   console.log(value+'-------test2--------');
+// })
+
+// test3
+// console.log('start');
+// let pp=new Promise(resolve=>{
+//   setTimeout(()=>{
+//     console.log('1S outPut');
+//     resolve('xiexie');
+//   },1000)
+// })
+// setTimeout(function(){
+//   pp.then((val)=>{
+//     console.log(val);
+//     console.log('Goby');
+//   })
+// },3000)
+
+
+// test4
+// console.log('start');
+// new Promise(resolve=>{
+//   console.log('hello');
+//   resolve('every');
+// }).then((val)=>{
+//   console.log(val);
+//   (function(){
+//     return new Promise(resolve=>{
+//       setTimeout(()=>{
+//         console.log('my name is ss');
+//         resolve('xxxxxxxxxxxxxxxxxxx')
+//       },2000)
+//     }).then(val=>{console.log(val);})
+//   })()
+//   return false
+// }).then(value=>{
+//     console.log(value+'ooiiuuyy');
+// })
+
+// test5
+// console.log('start');
+// new Promise(resolve=>{
+//   console.log('step1');
+//   setTimeout(()=>{
+//     resolve(100);
+//   },1000)
+// }).then(value=>{
+//   return new Promise(resolve=>{
+//     console.log('step1-1');
+//     setTimeout((value)=>{
+//       resolve(110);
+//     },1000)
+//   })
+// }).then(value=>{
+//   console.log('step1-2');
+//   return value;
+// }).then(value=>{
+//   console.log('step1-3');
+//   console.log(value);
+// }).then(value=>{
+//   console.log('end');
+// })
+
+// test6
+// new Promise(resolve=>{
+//   console.log(1);
+//   resolve()
+// }).then(()=>{
+//   console.log('success');
+// })
+// console.log('xxx');
+
+
+// test7
+// new Promise(resolve=>{
+//   setTimeout(()=>{
+//     console.log('start');
+//     resolve()
+//   },1000)
+// }).then(value=>{
+//   throw new Error('bug');
+// }).catch(value=>{
+//   console.log(value);
+//   throw new Error('raoguo')
+// }).then(value=>{
+//   console.log('catch houmian1');
+// }).then(value=>{
+//   console.log('catch houmian2');
+// }).catch(value=>{
+//   console.log('errorssss'+value);
+// })
+
+// (new function f(){
+//   this.a=2;
+//   return ((...b)=>{
+//     console.log(this.a);
+//   }).bind({a:5})
+// })()
